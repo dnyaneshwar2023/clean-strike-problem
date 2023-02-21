c7144c132dd95d6230256f2b11f5f1f522f8b37d
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-21 22:51:41 +0530

    Fix CoinsCollection

--- src/test/kotlin/models/PlayerTest.kt
+++ src/test/kotlin/models/PlayerTest.kt
@@ -4,7 +4,7 @@ class PlayerTest {
 
     @org.junit.jupiter.api.Test
     fun `it should create a player`() {
-        val player = Player("Dnyaneshwar", 1,1)
+        val player = Player("Dnyaneshwar", CoinsCollection(0, 0), 1)
 
         assert(player is Player)
     }
--- src/test/kotlin/models/StrikeTest.kt
+++ src/test/kotlin/models/StrikeTest.kt
@@ -8,7 +8,7 @@ class StrikeTest {
     @Test
     fun `it should create new turn of type strike`() {
         val strike =
-            Strike(pocketedCoins = CoinsCollection(1, 1), isStrikerFoul = false, outThrownCoins = CoinsCollection(0, 0))
+            Strike(pocketedCoins = CoinsCollection(1, 1), outThrownCoins = CoinsCollection(0, 0))
 
         assert(strike is Strike)
     }
