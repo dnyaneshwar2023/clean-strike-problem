544ebf24bfafa68851965daf2bc713cd7082b684
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-21 22:35:01 +0530

    Add function to validate turn coin amounts

---
+++ src/main/kotlin/exceptions/InsufficientCoinsException.kt
@@ -0,0 +1,3 @@
+package exceptions
+
+class InsufficientCoinsException(s: String) : Exception(s)
--- src/main/kotlin/models/PlayGround.kt
+++ src/main/kotlin/models/PlayGround.kt
@@ -1,5 +1,7 @@
 package models
 
+import exceptions.InsufficientCoinsException
+
 class PlayGround(val firstPlayer: Player, val secondPlayer: Player, val coins: CoinsCollection) {
 
     fun addCoins(coinsToAdd: CoinsCollection) {
@@ -7,14 +9,29 @@ class PlayGround(val firstPlayer: Player, val secondPlayer: Player, val coins: C
     }
 
     fun performTurn(player: Player, strike: Strike) {
+        validateTurn(strike)
 
         val coinsEarned = strike.calculateEarnedCoins()
-
         player.coins.addCoins(coinsEarned)
+
         player.points += strike.calculatePoints()
 
         val returnCoins = strike.calculateReturnCoins()
         coins.addCoins(returnCoins)
 
+    }
+
+    private fun validateTurn(strike: Strike) {
+        if (checkBlackCoinsOverflow(strike) || checkRedCoinsOverflow(strike)) {
+            throw InsufficientCoinsException("Invalid turn, Not insufficient coins in board to pocket")
+        }
+    }
+
+    private fun checkRedCoinsOverflow(strike: Strike): Boolean {
+        return coins.redCoins < strike.pocketedCoins.redCoins + strike.outThrownCoins.redCoins
+    }
+
+    private fun checkBlackCoinsOverflow(strike: Strike): Boolean {
+        return coins.blackCoins < strike.pocketedCoins.blackCoins + strike.outThrownCoins.blackCoins
     }
 }
